758cec298c6019aea8c23cd61b817801a23b7f45
paliwal999harsh <paliwal999harsh@gmail.com>
2023-07-29 00:51:03 +0530

    Initial commit: Set up project structure and dependencies.

---
+++ src/main/java/com/paliwal999harsh/competitivecodehub/CompetitiveCodeHubApplication.java
@@ -0,0 +1,13 @@
+package com.paliwal999harsh.competitivecodehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompetitiveCodeHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompetitiveCodeHubApplication.class, args);
+	}
+
+}

aab2c619be8a143e52d5f8434a30e2e93e1cee79
paliwal999harsh <paliwal999harsh+github@gmail.com>
2023-09-09 21:31:48 +0000

    Automated build # 227.

---
+++ src/main/java/com/paliwal999harsh/competitivecodehub/service/UserService.java
@@ -0,0 +1,15 @@
+package com.paliwal999harsh.competitivecodehub.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.Data;
+
+/**
+ * UserService
+ */
+@Data
+@Service
+public class UserService {
+    
+
+}
